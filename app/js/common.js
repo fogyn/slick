2147491ede9107dd2cac91abfa39607c72945e66
Sergey <SLSidorov@gmail.com>
2021-03-20 11:27:49 +0300

    slick bug
    Signed-off-by: Sergey <SLSidorov@gmail.com>

---
+++ app/js/common.js
@@ -0,0 +1,14 @@
+$('.single-item').slick({
+    arrows: true,
+    dots: true,
+    infinite: false,
+    autoplay: true,
+    autoplayspeed: 1000,
+
+    //prevArrow: '<div class="slider-arrow slider-arrow_left"></div>',
+    //nextArrow: '<div class="slider-arrow slider-arrow_right"></div>',
+  
+    
+});
+
+
